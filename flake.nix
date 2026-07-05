d53d0cf9152aa235363dd1522bb3f04e62a0c633
morvoxa <morvoxadev@gmail.com>
2026-07-05 07:38:13 +0000

    remove not used

--- flake.nix
+++ flake.nix
@@ -4,7 +4,6 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-26.05";
     xlibre-overlay.url = "git+https://codeberg.org/takagemacoed/xlibre-overlay?ref=dev-for-26.05";
-    nix-cachyos-kernel.url = "github:xddxdd/nix-cachyos-kernel/release";
     river-kwm.url = "github:rowsred/river_kwm_modules_nixos";
     fenix = {
       url = "github:nix-community/fenix";
@@ -28,14 +27,6 @@
           specialArgs = { inherit river-kwm fenix xlibre-overlay; };
           system = "x86_64-linux";
           modules = [
-            (
-              { pkgs, ... }:
-              {
-                nix.settings.substituters = [ "https://attic.xuyh0120.win/lantian" ];
-                nix.settings.trusted-public-keys = [ "lantian:EeAUQ+W+6r7EtwnmYjeVwx5kOGEBpjlBfPlzGlTNvHc=" ];
-                boot.kernelPackages = nix-cachyos-kernel.legacyPackages.x86_64-linux.linuxPackages-cachyos-latest;
-              }
-            )
             ./src/configuration.nix
             ./src/dwm-wm.nix
             ./src/files-manager.nix
--- src/cosmic-de.nix
+++
@@ -1,9 +0,0 @@
-{ pkgs, ... }: {
-  services.displayManager.cosmic-greeter.enable = true;
-  services.desktopManager.cosmic.enable = true;
-
-  environment.systemPackages = with pkgs; [
-    firefox
-    alacritty
-  ];
-}
