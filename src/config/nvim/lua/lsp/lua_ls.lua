60ccbe546032d315255b923b8642334f50690088
morvoxa <morvoxadev@gmail.com>
2026-07-07 06:09:56 +0000

    update config

--- src/config/nvim/lua/lsp/lua_ls.lua
+++ src/config/nvim/lua/lsp/lua_ls.lua
@@ -10,4 +10,34 @@ vim.lsp.config("lua_ls", {
 		},
 	},
 })
+
+vim.lsp.config("tailwindcss", {
+	filetypes = {
+		"html",
+		"css",
+		"javascript",
+		"typescript",
+		"react",
+		"rust",
+	},
+
+	init_options = {
+		userLanguages = {
+			rust = "html",
+		},
+	},
+
+	settings = {
+		tailwindCSS = {
+			experimental = {
+				classRegex = {
+					-- Mencari pola class: "..." di dalam file rust
+					[[class:\s*"([^"]*)"]],
+					[[classname:\s*"([^"]*)"]],
+				},
+			},
+		},
+	},
+})
 vim.lsp.enable("lua_ls")
+vim.lsp.enable("tailwindcss")
