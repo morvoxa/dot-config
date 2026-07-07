7072e122a65d3bcdf991a733a5a223738fc2fe43
morvoxa <morvoxadev@gmail.com>
2026-07-07 13:32:13 +0000

    dev

--- src/config/nvim/lua/lazy-reg.lua
+++ src/config/nvim/lua/lazy-reg.lua
@@ -22,4 +22,52 @@ require("lazy").setup({
 	{ "mrcjkb/rustaceanvim" },
 	{ "folke/snacks.nvim" },
 	{ "romus204/tree-sitter-manager.nvim" },
+	{
+		"folke/flash.nvim",
+		event = "VeryLazy",
+		---@type Flash.Config
+		opts = {},
+		keys = {
+			{
+				"s",
+				mode = { "n", "x", "o" },
+				function()
+					require("flash").jump()
+				end,
+				desc = "Flash",
+			},
+			{
+				"S",
+				mode = { "n", "x", "o" },
+				function()
+					require("flash").treesitter()
+				end,
+				desc = "Flash Treesitter",
+			},
+			{
+				"r",
+				mode = "o",
+				function()
+					require("flash").remote()
+				end,
+				desc = "Remote Flash",
+			},
+			{
+				"R",
+				mode = { "o", "x" },
+				function()
+					require("flash").treesitter_search()
+				end,
+				desc = "Treesitter Search",
+			},
+			{
+				"<c-s>",
+				mode = { "c" },
+				function()
+					require("flash").toggle()
+				end,
+				desc = "Toggle Flash Search",
+			},
+		},
+	},
 })
