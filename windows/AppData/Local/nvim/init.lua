ce59732de3f363d54a16a931ebe29c0b7f46223e
morvoxa <morvoxadev@gmail.com>
2026-07-12 11:50:22 -0700

    neovim add more config

--- windows/AppData/Local/nvim/init.lua
+++ windows/AppData/Local/nvim/init.lua
@@ -1,107 +1,44 @@
 local o = vim.opt
-local map = vim.api.nvim_set_keymap
+local k = vim.keymap
 vim.g.mapleader = " "
---========================================================
-o.number = true
-o.tabstop = 4
-o.shiftwidth = 4
-o.relativenumber = true
-o.splitright = true
-o.clipboard = "unnamedplus"
-vim.cmd([[colorscheme catppuccin]])
---========================================================
-map("i", "jk", "<esc>", {})
-map("n", "<leader>w", ":w<cr>", {})
-map("n", "<leader>nh", ":nohl<cr>", {})
-map("n", "<leader>x", ":bdel<cr>", {})
-map("n", "<leader>c", ":belowright 15 split | term ", { silent = false })
-map("n", "<leader>ff", ":FzfLua files<cr>", { silent = false })
-map("n", "<leader>e", ":Ex<cr>", { silent = false })
-map("v", "<Tab>", ">gv", { desc = "Indent ke kanan" })
-map("v", "<S-Tab>", "<gv", { desc = "Indent ke kiri" })
-
+o.number = true          -- Menampilkan nomor baris
+o.relativenumber = true  -- Menampilkan nomor baris relatif (mempercepat lompat baris)
+o.tabstop = 4            -- Jumlah spasi yang dihitung untuk satu Tab
+o.shiftwidth = 4         -- Jumlah spasi untuk indentasi otomatis
+o.expandtab = true       -- Mengubah tombol Tab menjadi spasi (standar modern)
+o.smartindent = true     -- Indentasi otomatis yang lebih cerdas mengikuti struktur kode
+o.wrap = false           -- Mencegah baris kode panjang otomatis turun ke bawah
+o.ignorecase = true      -- Mengabaikan huruf besar/kecil saat mencari teks...
+o.smartcase = true       -- ...KECUALI jika Anda mengetik huruf besar secara eksplisit
+o.incsearch = true       -- Menyorot teks yang cocok secara langsung saat baru mengetik
+o.termguicolors = true   -- Mengaktifkan warna 24-bit (wajib untuk tema modern)
+o.splitright = true      -- Vsplit otomatis membuka jendela baru di sebelah kanan
+o.clipboard = "unnamedplus" -- Menghubungkan clipboard Neovim dengan clipboard OS
+o.signcolumn = "yes"     -- Selalu munculkan kolom ikon di kiri (mencegah layar "lompat")
+o.updatetime = 250       -- Mempercepat trigger event (LSP, Git Gutter, dll.)
+o.timeoutlen = 300       -- Mempercepat kombinasi tombol berantai (seperti leader)
+o.scrolloff = 8          -- Menjaga jarak 8 baris tersisa di atas/bawah saat scroll
+o.autoread = true        -- Otomatis reload file jika diubah di luar Neovim (misal oleh git)
+o.swapfile = false       -- Mematikan pembuatan file .swp yang mengotori folder kerja
+o.backup = false         -- Mematikan backup file otomatis sebelum disimpan
+k.set("i", "jk", "<esc>", { desc = "Keluar dari mode insert dengan cepat" })
+k.set("n", "<leader>w", ":w<cr>", { desc = "Simpan file" })
+k.set("n", "<leader>nh", ":nohl<cr>", { desc = "Hapus sorotan pencarian" })
+k.set("n", "<leader>x", ":bdel<cr>", { desc = "Tutup buffer/tab aktif" })
+k.set("n", "<leader>e", ":Ex<cr>", { desc = "Buka file explorer bawaan (Netrw)" })
+k.set("n", "<leader>ff", ":FzfLua files<cr>", { desc = "Buka file explorer bawaan (Netrw)" })
+k.set("n", "<leader>c", ":belowright 15 split | term ", { desc = "Buka terminal di bawah" })
+k.set("v", "<Tab>", ">gv", { desc = "Indent ke kanan" })
+k.set("v", "<S-Tab>", "<gv", { desc = "Indent ke kiri" })
 vim.pack.add({
-	{ src = "https://github.com/windwp/nvim-autopairs" },
-	{ src = "https://github.com/folke/flash.nvim" },
-	{ src = "https://github.com/ibhagwan/fzf-lua" },
-	{ src = "https://github.com/romus204/tree-sitter-manager.nvim" },
-	{ src = "https://github.com/stevearc/conform.nvim" },
+    {src="https://github.com/folke/flash.nvim"},
+    {src="https://github.com/windwp/nvim-autopairs"},
+    {src="https://github.com/ibhagwan/fzf-lua"},
 })
 
---========================================================
-require("tree-sitter-manager").setup({
-	auto_install = true,
-})
---========================================================
-require("nvim-autopairs").setup({})
---========================================================
-require("flash").setup({})
-vim.keymap.set({ "n", "x", "o" }, "s", function()
-	require("flash").jump()
-end, { desc = "Flash" })
-vim.keymap.set({ "n", "x", "o" }, "S", function()
-	require("flash").treesitter()
-end, { desc = "Flash Treesitter" })
-vim.keymap.set("o", "r", function()
-	require("flash").remote()
-end, { desc = "Remote Flash" })
-vim.keymap.set({ "x", "o" }, "R", function()
-	require("flash").treesitter_search()
-end, { desc = "Treesitter Search" })
-vim.keymap.set({ "c" }, "<c-s>", function()
-	require("flash").toggle()
-end, { desc = "Toggle Flash Search" })
---========================================================
-require("conform").setup({
-	formatters_by_ft = {
-		lua = { "stylua" },
-		c = { "clang-format" },
-		cpp = { "clang-format" },
-		cmake = { "gersemi" },
-		zig = { "zigfmt" },
-		rust = { "rustfmt" },
-	},
-	format_on_save = {
-		lsp_format = false,
-	},
-})
---========================================================
-local function get_formatter_status()
-	local bufnr = vim.api.nvim_get_current_buf()
-	local ft = vim.bo[bufnr].filetype
-
-	-- Abaikan jika buffer kosong atau bukan file biasa (misal: terminal, NvimTree)
-	if ft == "" or vim.bo[bufnr].buftype ~= "" then
-		return ""
-	end
-
-	local fmt_list = {}
-	local status, conform = pcall(require, "conform")
-
-	if status then
-		-- Mengambil daftar formatter yang tersedia untuk buffer saat ini
-		local formatters = conform.list_formatters(bufnr)
-		for _, f in ipairs(formatters) do
-			table.insert(fmt_list, f.name)
-		end
-	end
-
-	-- Format tampilan statusline
-	if #fmt_list > 0 then
-		return " ▼ {" .. table.concat(fmt_list, ", ") .. "}"
-	else
-		return " [!] {!No Formatter}"
-	end
-end
-
-function MyStatusLine()
-	local file_name = " %f %M "
-	local align = "%=" -- Geser semua komponen setelah ini ke kanan
-	local fmt_info = get_formatter_status()
-	local location = " %l:%c %P "
-
-	return string.format("%s%s%s%s", file_name, align, fmt_info, location)
-end
-
-vim.opt.statusline = "%!v:lua.MyStatusLine()"
---========================================================
+require("nvim-autopairs").setup {}
+k.set({ "n", "x", "o" }, "s", function() require("flash").jump() end, { desc = "Flash" })
+k.set({ "n", "x", "o" }, "S", function() require("flash").treesitter() end, { desc = "Flash Treesitter" })
+k.set("o", "r", function() require("flash").remote() end, { desc = "Remote Flash" })
+k.set({ "o", "x" }, "R", function() require("flash").treesitter_search() end, { desc = "Treesitter Search" })
+k.set("c", "<c-s>", function() require("flash").toggle() end, { desc = "Toggle Flash Search" })
--- windows/dot_wezterm.lua
+++ windows/dot_wezterm.lua
@@ -1,23 +1,33 @@
 local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
--- Define the coreutils we want to unalias so Scoop takes over
 local unalias_script = [[
   $utils = @('ls','cat','chmod','cp','cut','date','echo','env','id','ln','mkdir','mv','pwd','rm','rmdir','sleep','sort','tail','tee','touch','uname','uniq','wc');
   foreach ($u in $utils) { if (Get-Alias $u -EA SilentlyContinue) { Remove-Item "alias:\$u" -Force } }
 ]]
 
+local safe_msvc_script = [[
+  try {
+      $vsModule = 'C:\Program Files\Microsoft Visual Studio\18\Community\Common7\Tools\Microsoft.VisualStudio.DevShell.dll'
+      if (Test-Path $vsModule) {
+          Import-Module $vsModule -ErrorAction Stop
+          Enter-VsDevShell -VsInstallPath 'C:\Program Files\Microsoft Visual Studio\18\Community' -Arch x64 -ErrorAction Stop
+          Write-Host "[WezTerm] MSVC DevShell successfully loaded." -ForegroundColor Green
+      } else {
+          Write-Warning "[WezTerm] MSVC DevShell dll not found. Skipping MSVC setup."
+      }
+  } catch {
+      Write-Warning "[WezTerm] Failed to initialize MSVC DevShell. Error: $_"
+  }
+]]
+
 config.default_prog = {
-	"powershell.exe",
-	"-NoExit",
-	"-Command",
-	-- 1. Load VS Developer Shell
-	"Import-Module 'C:\\Program Files\\Microsoft Visual Studio\\18\\Community\\Common7\\Tools\\Microsoft.VisualStudio.DevShell.dll'; "
-		.. "Enter-VsDevShell -VsInstallPath 'C:\\Program Files\\Microsoft Visual Studio\\18\\Community' -Arch x64; "
-		-- 2. Run the unalias script inline
-		.. unalias_script,
+    "powershell.exe",
+    "-NoExit",
+    "-Command",
+    -- Menggunakan string baru (\n) saat menggabungkan agar syntax PowerShell tidak rusak
+    safe_msvc_script .. "\n" .. unalias_script,
 }
-config.font = wezterm.font("SFMono Nerd Font", { weight = "Regular", italic = false })
 
 config.default_cwd = wezterm.home_dir
 
