70da5beec8250839a872d37def1332d6b77b12dd
morvoxa <morvoxadev@gmail.com>
2026-07-19 03:37:59 -0700

    update config

--- windows/AppData/Local/nvim/lua/plugins/etc.lua
+++
@@ -1,8 +0,0 @@
-vim.notify = require("fidget").notify
-require("fidget").setup({})
-require("luasnip.loaders.from_vscode").load({})
-require("nvim-autopairs").setup({})
-require("tree-sitter-manager").setup({
-	auto_install = true,
-	highlight = true,
-})
