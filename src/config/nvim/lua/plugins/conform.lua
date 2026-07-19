b14f5ddecb9139ba8152551a850ae4e579689337
morvoxa <morvoxadev@gmail.com>
2026-07-19 11:49:08 +0000

    clang-format

--- src/config/nvim/lua/plugins/conform.lua
+++ src/config/nvim/lua/plugins/conform.lua
@@ -3,10 +3,17 @@ require("conform").setup({
 		["dioxus"] = {
 			cmd = { "dx", "fmt", "--file", "$FILENAME" },
 		},
+		["clang-format"] = {
+			prepend_args = { "--style=Google" },
+		},
 	},
 	formatters_by_ft = {
+		c = { "clang-format" },
+		cpp = { "clang-format" },
+		h = { "clang-format" },
 		lua = { "stylua" },
 		rust = { "dioxus", "rustfmt" },
+		toml = { "taplo" },
 	},
 	format_on_save = {
 		timeout_ms = 500,
