4801ba542d3849f68b0b0e5fc9d163f54b563414
morvoxa <morvoxadev@gmail.com>
2026-07-23 13:21:25 +0700

    add oil config

--- src/config/nvim/lua/plugins/etc.lua
+++ src/config/nvim/lua/plugins/etc.lua
@@ -17,4 +17,22 @@ vim.g.rustaceanvim = {
 	},
 }
 require("luasnip.loaders.from_vscode").lazy_load()
-require("oil").setup()
+require("oil").setup({
+	float = {
+		padding = 0,
+		border = "rounded",
+
+		override = function(conf)
+			local total_cols = vim.o.columns
+			local width = math.floor(total_cols * 0.35)
+			local total_lines = vim.o.lines
+			local height = math.floor(total_lines * 0.85) -- Adjust 0.85 to change vertical size
+			local row = math.floor((total_lines - height) / 2)
+			conf.row = row
+			conf.col = total_cols - width - 2
+			conf.width = width
+			conf.height = height
+			return conf
+		end,
+	},
+})
