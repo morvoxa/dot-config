f8e87b15807b859e8b74416f52a45fa8af3bb0c2
morvoxa <morvoxadev@gmail.com>
2026-07-26 16:56:55 +0700

    base-apps

--- User/default.nix
+++ User/default.nix
@@ -11,11 +11,15 @@
   home.stateVersion = "26.05";
   programs.home-manager.enable = true;
   home.packages = with pkgs; [
+    firefox
     fastfetch
     neovim
     git
     fzf
     nixfmt
     tree
+    xclip
+    alacritty
+    vscodium
   ];
 }
