06fe34fa399f58e1bd49fa29e38d8c93ed8b9b01
morvoxa <morvoxadev@gmail.com>
2026-07-27 22:55:03 +0700

    cfce upgrade

--- System/xfce.nix
+++ System/xfce.nix
@@ -1,5 +1,8 @@
-{ ... }: {
+{ pkgs, ... }: {
   services.displayManager.ly.enable = true;
   services.xserver.desktopManager.xfce.enable = true;
   services.xserver.enable = true;
+  environment.systemPackages = with pkgs; [
+    xfce4-netload-plugin
+  ];
 }
--- justfile
+++ justfile
@@ -2,5 +2,7 @@ default:
   @just --list
 wsl:
   home-manager switch --flake .#wsl
-desktop:
+des:
   home-manager switch --flake .#desktop
+os:
+  sudo nixos-rebuild switch --flake .
