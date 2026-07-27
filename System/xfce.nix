54ead48c75f35808430631787e931552cc046954
morvoxa <morvoxadev@gmail.com>
2026-07-27 22:56:46 +0700

    xfce

--- System/xfce.nix
+++ System/xfce.nix
@@ -1,8 +1,5 @@
-{ pkgs, ... }: {
+{ ... }: {
   services.displayManager.ly.enable = true;
   services.xserver.desktopManager.xfce.enable = true;
   services.xserver.enable = true;
-  environment.systemPackages = with pkgs; [
-    xfce4-netload-plugin
-  ];
 }
