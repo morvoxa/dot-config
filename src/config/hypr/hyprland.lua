448a59b58c9be7d892dfa485375c19df1f55d099
morvoxa <morvoxadev@gmail.com>
2026-07-29 15:30:03 +0700

    update config

--- src/config/hypr/hyprland.lua
+++ src/config/hypr/hyprland.lua
@@ -87,7 +87,7 @@ hl.config({
 		gaps_in = 0,
 		gaps_out = 0,
 
-		border_size = 2,
+		border_size = 0,
 
 		col = {
 			active_border = { colors = { "rgba(33ccffee)", "rgba(00ff99ee)" }, angle = 45 },
