1f24a56a1a55963da8e1deaf6041f06d6b85d81a
morvoxa <morvoxadev@gmail.com>
2026-08-05 11:22:57 +0700

    update config

--- src/config/nvim/lua/config/keymaps.lua
+++ src/config/nvim/lua/config/keymaps.lua
@@ -1,7 +1,6 @@
 local map = vim.keymap.set
 map("i", "jk", "<Esc>", { desc = "Exit Insert Mode" })
 map("n", "<leader>w", ":w<cr>", { desc = "Save File" })
-map("n", "<leader>c", ":bdel<cr>", { desc = "Delete Buffer" })
 map("n", "<leader>1", "<C-w>w", { desc = "Switch Window" })
 
 local function toggle_lsp_features()
